f5d11d48a9b16ec8af80d3653ed3c74fb1880acf
David Gal Abitbul <52243163+galabitbul8@users.noreply.github.com>
2021-04-12 21:12:28 +0300

    create move object
    paddle, ball , paddle move

--- app/src/main/java/com/galab_rotemle/ex2/BrickCollection.java
+++ app/src/main/java/com/galab_rotemle/ex2/BrickCollection.java
@@ -1,6 +1,7 @@
 package com.galab_rotemle.ex2;
 
 import android.graphics.Canvas;
+import android.util.Log;
 
 import java.util.Random;
 
@@ -20,26 +21,34 @@ public class BrickCollection {
         this.height = height;
         this.width = width;
         this.rows = (int)(Math.random()*(MAX_ROWS+1-MIN_ROWS))+MIN_ROWS;
-        this.columns = (int)(Math.random()*(MAX_COLUMNS+1-MIN_COLUMNS))+MAX_COLUMNS;
+        this.columns = (int)(Math.random()*(MAX_COLUMNS+1-MIN_COLUMNS))+MIN_COLUMNS;
         this.bricks = new Brick[this.rows][this.columns];
-    }
-    public void draw(Canvas canvas){
-        float x1=0,x2=5,y1=0,y2=0;
+        Log.i("test",this.rows + " - " + this.columns);
+
+
+        float x1=0,x2=0,y1=0,y2=0;
 
         for(int i=0;i<this.rows;i++){
-            x1 = x2 + 10;
+            x1 = x2 + 5;
             x2 = (((this.width-5)/this.rows)*(i+1));
             y2 =250;
             for (int j=0;j<this.columns;j++){
-                y1 = y2 + 10;
+                y1 = y2 + 5;
                 y2 = (250 + (this.height/20)*(j+1));
                 this.bricks[i][j] = new Brick( x1, x2, y1, y2);
+            }
+        }
+        this.brickHeight = this.height/20;
+        this.brickWidth = x2-x1;
+        Log.i("test",this.brickHeight + " - " + this.brickWidth);
+    }
+
+    public void draw(Canvas canvas){
+        for(int i=0;i<this.rows;i++){
+            for (int j=0;j<this.columns;j++){
                 this.bricks[i][j].draw(canvas);
             }
-
         }
-        this.brickHeight = y2-y1;
-        this.brickWidth = x2-x1;
     }
 
     public float getBrickHeight() {
--- app/src/main/java/com/galab_rotemle/ex2/GameView.java
+++ app/src/main/java/com/galab_rotemle/ex2/GameView.java
@@ -11,20 +11,29 @@ import android.view.View;
 
 
 public class GameView extends View {
+    private static final int GET_READY = 1;
+    private static final int PLAYING = 2;
+    private static final int GAME_OVER = 3;
     private Paint score;
     private Paint lives;
-    private Paint life;
-    private int scoreNum;
+    private Paint life,lifeIn,lifeOut;
+    private int scoreNum,lifesNumber, state;
     private int height,width;
+    private float tempX;
     private BrickCollection bricks;
     private Ball ball;
     private Paddle paddle;
+    private boolean toched;
     private float brickWidth,brickHeight;
     private Paint startGame, finishGame;
+    private Thread thread;
 
     public GameView(Context context,AttributeSet attrs) {
         super(context, attrs);
+        state =1;
         scoreNum=0;
+        toched = false;
+        lifesNumber = 3;
         score = new Paint();
         score.setColor(Color.GREEN);
         score.setTextSize(75);
@@ -34,17 +43,29 @@ public class GameView extends View {
         lives.setTextSize(75);
         lives.setTextAlign(Paint.Align.RIGHT);
 
-        // TODO: fill the life with white color when the player have the life and remove white and put שקוף if the player loss the life he had
+        lifeIn = new Paint();
+        lifeIn.setColor(Color.WHITE);
+        lifeIn.setTextSize(70);
+        lifeIn.setTextAlign(Paint.Align.RIGHT);
+
+        lifeOut = new Paint();
+        lifeOut.setColor(Color.rgb(66,66,66));
+        lifeOut.setTextSize(70);
+        lifeOut.setTextAlign(Paint.Align.RIGHT);
+
         life = new Paint();
         life.setColor(Color.GREEN);
 
         startGame = new Paint();
         startGame.setColor(Color.GREEN);
         startGame.setTextSize(75);
+        startGame.setTextAlign(Paint.Align.CENTER);
 
         finishGame = new Paint();
         finishGame.setColor(Color.GREEN);
         finishGame.setTextSize(75);
+        finishGame.setTextAlign(Paint.Align.CENTER);
+
     }
 
     @Override
@@ -57,6 +78,31 @@ public class GameView extends View {
         canvas.drawCircle(getWidth() - 110,80,30,life);
         canvas.drawCircle(getWidth() - 180,80,30,life);
 
+        switch (lifesNumber){
+            case 0:
+                canvas.drawCircle(getWidth() - 40,80,25,lifeOut);
+                canvas.drawCircle(getWidth() - 110,80,25,lifeOut);
+                canvas.drawCircle(getWidth() - 180,80,25,lifeOut);
+                break;
+            case 1:
+                canvas.drawCircle(getWidth() - 40,80,25,lifeIn);
+                canvas.drawCircle(getWidth() - 110,80,25,lifeOut);
+                canvas.drawCircle(getWidth() - 180,80,25,lifeOut);
+                break;
+            case 2:
+                canvas.drawCircle(getWidth() - 40,80,25,lifeIn);
+                canvas.drawCircle(getWidth() - 110,80,25,lifeIn);
+                canvas.drawCircle(getWidth() - 180,80,25,lifeOut);
+                break;
+            case 3:
+                canvas.drawCircle(getWidth() - 40,80,25,lifeIn);
+                canvas.drawCircle(getWidth() - 110,80,25,lifeIn);
+                canvas.drawCircle(getWidth() - 180,80,25,lifeIn);
+                break;
+        }
+
+
+
         // draw the bricks collection
         this.bricks.draw(canvas);
 
@@ -64,10 +110,24 @@ public class GameView extends View {
         this.ball.draw(canvas);
         this.paddle.draw(canvas);
 
-        //when the player start to play TODO:put the text in the middle when the playar start play
-//        canvas.drawText("Click to PLAY!",this.width/2,this.height/2,this.startGame);
-        //when the player finish to play
-//        canvas.drawText("GAME OVER - You Loss!",this.width/2,this.height/2,this.startGame);
+        switch (state){
+            case GET_READY:
+                //when the player start to play
+                canvas.drawText("Click to PLAY!",this.width/2,this.height/2,this.startGame);
+                break;
+            case PLAYING:
+                break;
+            case GAME_OVER:
+                //when the player finish to play
+                canvas.drawText("GAME OVER - You Loss!",this.width/2,this.height/2,this.startGame);
+                break;
+        }
+
+        if(state == PLAYING)
+            if(toched){
+                this.paddle.movePaddle(tempX,this.width);
+            }
+
     }
 
     @Override
@@ -83,23 +143,33 @@ public class GameView extends View {
         this.brickHeight = this.bricks.getBrickHeight();
 
         //TODO: fix size of paddle and of the ball
-        this.paddle = new Paddle(this.width/2-brickWidth/2,this.height-150-this.brickHeight,this.width/2+this.brickWidth/2, this.height-150);
-        this.ball = new Ball(this.width/2,this.height-150-this.brickHeight-this.brickHeight,this.brickHeight/2);
+        this.paddle = new Paddle((float)this.width/2-brickWidth/2,(float)this.width/2+brickWidth/2,(float)this.height-150-this.brickHeight/2,(float)this.height-150);
+        this.ball = new Ball(this.width/2,this.height-150-brickHeight,brickHeight/2);
 
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        float tempX = event.getX();
-        float tempY = event.getY();
+        this.tempX = event.getX();
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
+                if(state == GET_READY)
+                    state = PLAYING;
+                if(state == PLAYING)
+                    this.toched = true;
+                invalidate();
                 break;
             case MotionEvent.ACTION_MOVE:
+                if(state == PLAYING)
+                    this.toched = true;
+                invalidate();
                 break;
             case MotionEvent.ACTION_UP:
+                if(state == PLAYING)
+                    this.toched = false;
                 break;
         }
+
 
         return true;
     }
